c58c896ebed52bdf71a54bf360911b8f385b6be9
DanielMarchenaM <danielmm0596@gmail.com>
2025-12-30 17:55:59 -0600

    Adding finished projects

---
+++ projects/01-tw-followCard/src/TwitterFollowCard.jsx
@@ -0,0 +1,26 @@
+
+function TwitterFollowCard() {
+    <>
+        <article className="tw-followCard">
+
+            <header className="tw-followCard-header">
+                <img src="https://unavatar.io/github/mdo" alt="" className="tw-followCard-avatar" />
+                <div className="tw-followCard-info">
+                    <strong>
+                        Bartender A
+                    </strong>
+                    <span className="tw-followCard-infoUserName">
+                        @bartenderA
+                    </span>
+                </div>
+            </header>
+
+            <aside>
+                <button className="tw-followCard-button">Follow</button>
+            </aside>
+
+        </article>
+    </>
+}
+
+export default App
