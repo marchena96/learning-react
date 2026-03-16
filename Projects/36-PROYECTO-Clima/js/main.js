9f3ddc743fc7cf98973ed556ce13f9a941e74b7b
DanielMarchenaM <danielmm0596@gmail.com>
2026-03-15 18:59:18 -0600

    Fix: integrar subcarpeta al repositorio principal

--- Projects/36-PROYECTO-Clima/js/main.js
+++ Projects/36-PROYECTO-Clima/js/main.js
@@ -22,7 +22,8 @@ function buscarClima(e) {
     }
 
     //4. Consultar la API: Obtener una API Key
-
+    //! Aquí es donde llamas a la función para consultar la API: ESTABA FALLANDO POR ESTO
+    callToAPI(ciudad, pais);
 
     console.log('Buscando el clima ...');
 }
@@ -54,7 +55,78 @@ function displayErrorMessage(message) {
 
 //4. Function call to API
 function callToAPI(pais, ciudad) {
-    const API_KEY = 'fe2def66855116fa18212dbd82cd71d6';
 
+    // Partes del endpoint:
+    // 1. Base URL: La dirección del servidor
+    // 2. El parámetro de búsqueda (ciudad y país)
+    // 3. appid: Tu credencial de acceso
+    // 4. units: Para que te de grados Celsius (metric) y no Kelvin
 
+    const appId = 'fe2def66855116fa18212dbd82cd71d6';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}&units=metric`;
+
+    // Mostramos un spinner o mensaje de carga si quieres (opcional)
+    console.log("Conectando...");
+
+    console.log(url); // Imprimir para ver si se armó bien
+
+    // Luego haces el fetch(url)...
+    // Realizar la petición
+    fetch(url)
+        .then(response => response.json()) // Paso 1: Convertir a JSOn
+        .then(data => {
+            console.log(data);
+
+            if (data.cod === "404") {
+                displayErrorMessage('Ciudad no encontrada');
+                return;
+            }
+
+            //Paso 2: Mostrar resultado en el HTML
+            mostrarClima(data);
+        })
+        .catch(error => console.log('Error de red:', error));
+}
+
+function mostrarClima(datos) {
+    const { name, main: { temp, temp_max, temp_min } } = datos;
+
+    // Limpiar el HTML previo
+    resultado.innerHTML = '';
+
+    const nombreCiudad = document.createElement('p');
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add('font-bold', 'text-2xl', 'text-center', 'text-white');
+
+    // 4. Crear el elemento para la temperatura actual
+    const actual = document.createElement('p');
+    actual.innerHTML = `Actual: ${Math.round(temp)} &#8451;`; // Muestra grados Celsius
+    actual.classList.add('font-bold', 'text-6xl', 'text-center', 'text-white');
+
+    // 5. Crear el elemento para la temperatura MÁXIMA
+    const tempMaxima = document.createElement('p');
+    tempMaxima.innerHTML = `Max: ${Math.round(temp_max)} &#8451;`;
+    tempMaxima.classList.add('text-xl', 'text-center', 'text-white')
+
+    // 6. Crear el elemento para la temperatura MÍNIMA
+    const tempMinima = document.createElement('p');
+    tempMinima.innerHTML = `Min: ${Math.round(temp_min)} &#8451;`;
+    tempMinima.classList.add('text-xl', 'text-center', 'text-white');
+
+    // 7. Crear un contenedor para organizar los elementos
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+
+    // IMPORTANTE: Agregar todos los elementos al contenedor
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMaxima); // <-- Si no haces esto, no se verá
+    resultadoDiv.appendChild(tempMinima); // <-- Si no haces esto, no se verá
+
+    // 8. Inyectar el contenedor final en el DOM
+    resultado.appendChild(resultadoDiv);
+}
+
+function KelvinACentigrados(grados) {
+    return parseInt(grados - 273.15);
 }
